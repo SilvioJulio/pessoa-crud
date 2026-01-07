a459cfca1a7926bcc5460f2e123aed4419e955c2
Silvio Julio <sivicr3@hotmail.com>
2026-01-07 17:33:13 -0300

    restaurados os métodos da classe PessoaController

--- src/main/java/com/dbacademia/pessoa/controller/PessoaController.java
+++ src/main/java/com/dbacademia/pessoa/controller/PessoaController.java
@@ -6,22 +6,26 @@ import com.dbacademia.pessoa.service.PessoaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/pessoa")
+@RequestMapping("/pessoas")
 public class PessoaController {
 
     @Autowired
     private  PessoaService service;
 
    @PostMapping
-    public ResponseEntity<Pessoa> criarPessoa(@Valid @RequestBody Pessoa pessoa){
-       return  new ResponseEntity<>(service.criar(pessoa), HttpStatus.CREATED);
+    public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa){
+       return ResponseEntity.status(HttpStatus.CREATED)
+               .body(service.criar(pessoa));
+
+   }
+   @GetMapping
+   public ResponseEntity<Page<Pessoa>> listar (Pageable pageable){
+       return ResponseEntity.ok(service.listarTodos(pageable));
    }
 }
